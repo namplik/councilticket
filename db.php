7683802c3562e25fbe7b54d2705ddcababac57a7
namplik <nnnipa140@gmail.com>
2026-03-03 18:27:56 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,13 @@
 <?php
-
-$host = getenv("MYSQLHOST");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
+$host = "switchyard.proxy.rlwy.net";
+$port = "45554";
+$user = "root";
+$pass = "hMHPaJjqGSVlGNFvbOFQSTBNbUzSCiqa";
+$db   = "railway";
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
